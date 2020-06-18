1b66afadd0bee0017aa035779b83a94fc5eae09a
meharmiglani <meharkaur97@gmail.com>
2020-06-18 21:18:40 +0530

    Separated Admin and Student controllers

--- src/com/flipkart/controller/StudentController.java
+++ src/com/flipkart/controller/StudentController.java
@@ -2,148 +2,89 @@ package com.flipkart.controller;
 
 import java.util.List;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.xml.crypto.URIReferenceException;
 
 import org.apache.log4j.Logger;
 
 import com.flipkart.bean.Course;
 import com.flipkart.bean.Grade;
 import com.flipkart.bean.Payment;
-import com.flipkart.bean.Student;
-import com.flipkart.bean.User;
 import com.flipkart.service.StudentService;
 
 @Path("/student")
 public class StudentController {
-	
+
 	private static StudentService studentService = new StudentService();
 	private final static Logger logger = Logger.getLogger(StudentController.class);
-	
-	//Retrieve list of all students
-	@GET
-	@Path("/all")
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Student> getAllStudents(){
-		return studentService.getStudents();
-	}
-	
-	//Retrieve list of students' courses
+
+
+	// Retrieve list of students' courses
 	@GET
 	@Path("/courselist/{studentId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Course> getStudentCourses(@PathParam("studentId") int studentId){
+	public List<Course> getStudentCourses(@PathParam("studentId") int studentId) {
 		return studentService.getStudentCourses(studentId);
 	}
+
 	
-	//Create a new student
-	@POST
-	@Path("/post")
-	@Consumes("application/json")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response insertStudent(Student student){
-		int userId = student.getId();
-		String username = student.getUsername();
-		String password = student.getPassword();
-		int roleId = student.getRoleId();
-		User user = new User(userId, username, password, roleId);
-		studentService.createUser(user);
-		studentService.createStudent(student);
-		logger.info("Hit post service");
-		logger.info("Value of id from UI " + student.getId());
-		logger.info("Value of name from UI " + student.getName());
-		String result = "Track saved : " + student;
-		return Response.status(201).entity(result).build();
-	}
-	
-	//Delete a student
-	@DELETE
-	@Path("/delete/{studentId}")
-	public Response deleteStudent(@PathParam("studentId") int studentId)
-	throws URIReferenceException{
-		studentService.deleteUser(studentId);
-		studentService.deleteStudent(studentId);
-		logger.info("Student with id " + studentId + " deleted successfully");
-		return Response.status(200).entity("Track Id " + studentId + " successfully deleted").build();
-	}
-	
-	//Update student details
-	@PUT
-	@Path("/update/{studentId}")
-	@Consumes("application/json")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Student updateStudent(@PathParam("studentId") int studentId, Student student)
-	throws URIReferenceException{
-		if(studentService.updateStudent(studentId, student)){
-			logger.info("Student updated");
-		}
-		return student;
-	}
-	
-	public String getStudentName(int studentId){
-		return studentService.getStudentName(studentId);
-	}
-	
-	//Add a course
+	// Add a course
 	@POST
 	@Path("/addCourse/{studentId}/{courseId}")
-	public void addCourse(@PathParam("studentId") int studentId, @PathParam("courseId") int courseId){
+	public void addCourse(@PathParam("studentId") int studentId, @PathParam("courseId") int courseId) {
 
-        if(studentService.checkForRegistration(studentId)){
-            logger.info("Cannot add a course after final registration");
-            return;
-        }
+		if (studentService.checkForRegistration(studentId)) {
+			logger.info("Cannot add a course after final registration");
+			return;
+		}
 
-        if(!studentService.canAddCourse(studentId)){
-            return;
-        }
+		if (!studentService.canAddCourse(studentId)) {
+			return;
+		}
 
-        String studentName = getStudentName(studentId);
-        if(studentService.addCourse(studentId, studentName, courseId)) {
-            logger.info("Course successfully added!");
-        }
-    }
-	
-	//Delete a course
+		String studentName = studentService.getStudentName(studentId);
+		if (studentService.addCourse(studentId, studentName, courseId)) {
+			logger.info("Course successfully added!");
+		}
+	}
+
+	// Delete a course
 	@DELETE
 	@Path("/deleteCourse/{studentId}/{courseId}")
-	public void deleteCourse(@PathParam("studentId") int studentId, @PathParam("courseId") int courseId){
-		
-		if(studentService.checkForRegistration(studentId)){
-            logger.info("Cannot delete a course after final registration");
-            return;
-        }
+	public void deleteCourse(@PathParam("studentId") int studentId, @PathParam("courseId") int courseId) {
 
-        if(studentService.deleteCourse(studentId, courseId)){
-            logger.info("Course deleted successfully");
-        }else{
-            //Throw Exception
-            logger.error("Could not delete course");
-        }
+		if (studentService.checkForRegistration(studentId)) {
+			logger.info("Cannot delete a course after final registration");
+			return;
+		}
+
+		if (studentService.deleteCourse(studentId, courseId)) {
+			logger.info("Course deleted successfully");
+		} else {
+			// Throw Exception
+			logger.error("Could not delete course");
+		}
 	}
-	
-	//Submit Registration
+
+	// Submit Registration
 	@GET
 	@Path("/submitRegistration/{studentId}/{paymentId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Payment> submitRegistration(@PathParam("studentId") int studentId, @PathParam("paymentId") int paymentId){
+	public List<Payment> submitRegistration(@PathParam("studentId") int studentId,
+			@PathParam("paymentId") int paymentId) {
 		return studentService.submitRegistration(studentId, paymentId);
 	}
-	
-	//View Marks
+
+	// View Marks
 	@GET
 	@Path("/viewGrades/{studentId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Grade> getGradeList(@PathParam("studentId") int studentId){
+	public List<Grade> getGradeList(@PathParam("studentId") int studentId) {
 		return studentService.viewMarksByCourse(studentId);
 	}
 }
--- src/com/flipkart/service/StudentService.java
+++ src/com/flipkart/service/StudentService.java
@@ -1,4 +1,5 @@
 package com.flipkart.service;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -8,8 +9,6 @@ import org.apache.log4j.Logger;
 import com.flipkart.bean.Course;
 import com.flipkart.bean.Grade;
 import com.flipkart.bean.Payment;
-import com.flipkart.bean.Student;
-import com.flipkart.bean.User;
 import com.flipkart.dao.FetchReportCardDao;
 import com.flipkart.dao.FinalRegistrationDao;
 import com.flipkart.dao.PayFeeDao;
@@ -18,7 +17,7 @@ import com.flipkart.dao.StudentDao;
 
 public class StudentService {
 	private final static LocalDate localDate = LocalDate.now();
-    private final static LocalTime localTime = LocalTime.now();
+	private final static LocalTime localTime = LocalTime.now();
 	private static StudentDao studentDao = new StudentDao();
 	private static RegisterCourseDao registerCourseDao = new RegisterCourseDao();
 	private final static Logger logger = Logger.getLogger(StudentService.class);
@@ -26,109 +25,87 @@ public class StudentService {
 	private static PayFeeDao payFeeDao = new PayFeeDao();
 	private static FetchReportCardDao fetchReportCardDao = new FetchReportCardDao();
 
-	
-	public List<Student> getStudents(){
-		return studentDao.viewAllStudents();
-	}
-	
-	public List<Course> getStudentCourses(int studentId){
+
+	public List<Course> getStudentCourses(int studentId) {
 		return studentDao.viewRegisteredCourses(studentId);
 	}
-	
-	public void createStudent(Student student){
-		studentDao.insertStudent(student);
-	}
-	
-	public void createUser(User user){
-		studentDao.createUser(user);
-	}
-	
-	public boolean deleteUser(int userId){
-		return studentDao.deleteUser(userId);
-	}
-	
-	public boolean deleteStudent(int studentId){
-		return studentDao.deleteStudent(studentId);
-	}
-	
-	public boolean canAddCourse(int studentId){
-        int enrolledCourses = registerCourseDao.courseLimitCheck(studentId);
-        if(enrolledCourses == 4){
-            logger.info("You have reached the maximum course limit.");
-            return false;
-        }
-        return true;
-    }
 
-    public boolean checkForRegistration(int studentId){
-        return finalRegistrationDao.checkRegistration(studentId);
-    }
-	
-	public boolean updateStudent(int studentId, Student student){
-		return studentDao.updateStudent(studentId, student);
+	public boolean canAddCourse(int studentId) {
+		int enrolledCourses = registerCourseDao.courseLimitCheck(studentId);
+		if (enrolledCourses == 4) {
+			logger.info("You have reached the maximum course limit.");
+			return false;
+		}
+		return true;
+	}
+
+	public boolean checkForRegistration(int studentId) {
+		return finalRegistrationDao.checkRegistration(studentId);
+	}
+
+	public boolean addCourse(int studentId, String studentName, int courseId) {
+		if (registerCourseDao.addCourse(studentId, studentName, courseId)) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean deleteCourse(int studentId, int courseId) {
+		if (registerCourseDao.deleteCourse(studentId, courseId)) {
+			return true;
+		}
+		return false;
+	}
+
+	public List<Payment> submitRegistration(int studentId, int paymentId) {
+		if (finalRegistrationDao.checkRegistration(studentId)) {
+			logger.info("You are already registered");
+			return null;
+		}
+
+		int enrolledCourses = registerCourseDao.courseLimitCheck(studentId);
+		if (enrolledCourses < 4) {
+			logger.info("Please select 4 courses to submit your registration. You are currently enrolled in "
+					+ enrolledCourses + " courses");
+			return null;
+		}
+
+		// Pay Fee
+		logger.info("Please pay the fee in order to confirm your registration.");
+		List<Payment> feeList = payFeeDao.fetchCourseFee(studentId);
+		double fee = 0;
+		logger.info("Fee payable: " + fee);
+
+		double scholarshipAmount = payFeeDao.getScholarshipAmount(studentId);
+
+		if (scholarshipAmount != -1) {
+			double reduction = (scholarshipAmount / 100) * fee;
+			fee -= reduction;
+		}
+
+		logger.info("Scholarship Amount for student " + studentId + " = " + scholarshipAmount + "%");
+		logger.info("Fee after scholarship: " + fee);
+
+		logger.info("Total fee paid: " + fee + "on " + localDate + " " + localTime.getHour() + ":"
+				+ localTime.getMinute() + " " + localDate.getDayOfWeek());
+
+		if (payFeeDao.insertIntoRegistration(studentId, paymentId, fee)) {
+			finalRegistrationDao.registerStudent(studentId);
+		}
+
+		return feeList;
+	}
+
+	public List<Grade> viewMarksByCourse(int studentId) {
+		if (!finalRegistrationDao.checkRegistration(studentId)) {
+			logger.info("Register to view your marks");
+			return null;
+		}
+
+		return fetchReportCardDao.getMarksByCourse(studentId);
 	}
 	
-	public String getStudentName(int studentId){
+	public String getStudentName(int studentId) {
 		return studentDao.getStudentName(studentId);
 	}
-	
-	public boolean addCourse(int studentId, String studentName, int courseId){
-		if(registerCourseDao.addCourse(studentId, studentName, courseId)){
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean deleteCourse(int studentId, int courseId){
-		if(registerCourseDao.deleteCourse(studentId, courseId)){
-			return true;
-		}
-		return false;
-	}
-	
-	public List<Payment> submitRegistration(int studentId, int paymentId){
-		if(finalRegistrationDao.checkRegistration(studentId)){
-            logger.info("You are already registered");
-            return null;
-        }
-
-        int enrolledCourses = registerCourseDao.courseLimitCheck(studentId);
-        if(enrolledCourses < 4){
-            logger.info("Please select 4 courses to submit your registration. You are currently enrolled in " + enrolledCourses + " courses");
-            return null;
-        }
-
-        //Pay Fee
-        logger.info("Please pay the fee in order to confirm your registration.");
-        List<Payment> feeList = payFeeDao.fetchCourseFee(studentId);
-        double fee = 0;
-        logger.info("Fee payable: " + fee);
-
-        double scholarshipAmount = payFeeDao.getScholarshipAmount(studentId);
-
-        if(scholarshipAmount != -1){
-            double reduction = (scholarshipAmount/100) * fee;
-            fee -= reduction;
-        }
-        
-        logger.info("Scholarship Amount for student " + studentId + " = " + scholarshipAmount + "%");
-        logger.info("Fee after scholarship: " + fee);
-
-        logger.info("Total fee paid: " + fee + "on " + localDate + " " + localTime.getHour() + ":" + localTime.getMinute() + " " + localDate.getDayOfWeek());
-
-        if(payFeeDao.insertIntoRegistration(studentId, paymentId, fee)){
-        	finalRegistrationDao.registerStudent(studentId);
-        }
-        
-        return feeList;
-	}
-	
-	public List<Grade> viewMarksByCourse(int studentId) {
-        if(!finalRegistrationDao.checkRegistration(studentId)){
-            logger.info("Register to view your marks");
-            return null;
-        }
-
-        return fetchReportCardDao.getMarksByCourse(studentId);
-    }
 }
